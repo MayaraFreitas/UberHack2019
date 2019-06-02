2c90a739ae298f157cab637bdd36abee1aa4dca8
MayaraFreitas <mah.alvesfreitas@hotmail.com>
2019-06-02 12:09:17 -0300

    MVP plataforma web

---
+++ fretop/js/lib/chart-js/chartjs-init.js
@@ -0,0 +1,39 @@
+﻿( function ( $ ) {
+	"use strict";
+
+	//radar chart
+	var ctx = document.getElementById( "radarChart" );
+	ctx.height = 221;
+	var myChart = new Chart( ctx, {
+		type: 'radar',
+		data: {
+			labels: [ "Iluminação", "Buraco na Via", "Acidente", "Assalto", "Mato na Via", "Sinalização", "Obstáculo", "Limpeza", "Alagamento", "Segurança" ],
+			datasets: [
+				{
+					label: "Mês Anterior",
+					data: [ 65, 32, 66, 45, 56, 37, 27, 41, 21, 33 ],
+					borderColor: "rgba(0, 123, 255, 0.6)",
+					borderWidth: "1",
+                    backgroundColor: "rgba(239, 83, 80, 0.42)"
+                            },
+				{
+					label: "Mês Atual",
+					data: [ 28, 12, 40, 19, 63, 21, 14, 21, 11, 7],
+					borderColor: "rgba(0, 123, 255, 0.7",
+					borderWidth: "1",
+					backgroundColor: "rgba(0, 123, 255, 0.5)"
+                            }
+                        ]
+		},
+		options: {
+			legend: {
+				position: 'top'
+			},
+			scale: {
+				ticks: {
+					beginAtZero: true
+				}
+			}
+		}
+	} );
+} )( jQuery );
---
+++ fretop/js/lib/morris-chart/dashboard1-init.js
@@ -0,0 +1,85 @@
+﻿$( function () {
+	"use strict";
+
+	Morris.Bar( {
+        element: 'chart-bairros',
+		data: [ {
+                diaSemana: 'Segunda',
+				tatuape: 0,
+				morumbi: 0,
+				santana: 90,
+				lapa: 0,
+				outros: 0
+        }, {
+                diaSemana: 'Terça',
+                tatuape: 10,
+                morumbi: 60,
+                santana: 40,
+                lapa: 80,
+                outros: 120
+        }, {
+                diaSemana: 'Quarfa',
+                tatuape: 120,
+                morumbi: 10,
+                santana: 90,
+                lapa: 30,
+                outros: 50
+        }, {
+                diaSemana: 'Quinta',
+                tatuape: 0,
+                morumbi: 0,
+                santana: 120,
+                lapa: 0,
+                outros: 0
+        }, {
+                diaSemana: 'Sexta',
+                tatuape: 0,
+                morumbi: 0,
+                santana: 0,
+                lapa: 150,
+                outros: 0
+        }, {
+                diaSemana: 'Sabado',
+                tatuape: 160,
+                morumbi: 75,
+                santana: 30,
+                lapa: 60,
+                outros: 90
+        }, {
+                diaSemana: 'Domingo',
+                tatuape: 10,
+                morumbi: 120,
+                santana: 40,
+                lapa: 60,
+                outros: 30
+        }
+
+
+        ],
+		colors: [ '#26DAD2', '#fc6180', '#62d1f3', '#ffb64d', '#4680ff' ],
+        xkey: 'diaSemana',
+        ykeys: ['tatuape', 'morumbi', 'santana', 'lapa', 'outros' ],
+        labels: ['Tatuape', 'Morumbi', 'Santana', 'Lapa', 'Outros' ],
+		pointSize: 0,
+		lineWidth: 0,
+		resize: true,
+		fillOpacity: 0.8,
+		behaveLikeLine: true,
+		gridLineColor: '#e0e0e0',
+		hideHover: 'auto'
+	} );
+
+    Morris.Donut({
+        element: 'donut_chart_1',
+        data: [
+            { label: 'Masculino', value: 60 },
+            { label: 'Feminino', value: 32 },
+            { label: 'Outros', value: 8 },
+        ],
+        colors: [
+            '#26DAD2', '#fc6180', '#ffb64d'
+        ],
+        formatter: function (y) { return y + "%" }
+    });
+
+} );
